5b5dc68d97a38565c9a1a7e311678033d8feb1f5
renatoroberto25 <renatoroberto25@yahoo.com.br>
2024-10-04 19:01:46 -0300

    já temos alguma coisa

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
 
 #My App
 app =  Flask(__name__)
 
-@app.route("/healthcheck")
+@app.route("/healtcheck")
 def index():
-    return "Site OK!"
+    return render_template("healtcheck.html")
 
 if __name__ in "__main__":
     app.run(debug=True)
