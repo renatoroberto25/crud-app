92af4a0ae9ccd0a1c95968230e93df8443a1a42f
renatoroberto25 <renatoroberto25@yahoo.com.br>
2024-10-12 12:23:08 -0300

    Novas coisas, como rotas e banco

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 #My App
 app =  Flask(__name__)
@@ -8,13 +9,45 @@ app =  Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
 
+class Mytask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"Task {self.id}"
+
+
 @app.route("/healtcheck")
 def healtcheck():
     return render_template("healtcheck.html")
 
-@app.route("/")
+# rotas
+# home
+@app.route("/",methods=["POST","GET"])
 def index():
-    return render_template("index.html") 
+    # add uma task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task =  Mytask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR:{e}")
+            return f"ERROR:{e}"
+    # Ver a tarefa atual
+    else:
+        tasks =  Mytask.query.order_by(Mytask.created).all()
+        return render_template('index.html', tasks=tasks)
+
+# deletando
+@app.route()
 
 if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
